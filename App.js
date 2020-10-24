d20a9bdb2956f8db4b4ab1aa4eb434e80efc5439
gnchau <40442415+gnchau@users.noreply.github.com>
2020-10-24 16:32:01 -0500

    Altru

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import LoginScreen from './screens/LoginScreen'
+import DashboardScreen from "./screens/DashboardScreen"
+import LoadingScreen from "./screens/LoadingScreen"
+
+import firebase from 'firebase'
+import { firebaseConfig } from './config'
+firebase.initializeApp(firebaseConfig)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppNavigator />
   );
 }
 
@@ -19,3 +24,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen : LoadingScreen,
+  LoginScreen:LoginScreen,
+  DashboardScreen:DashboardScreen,
+
+})
+
+const AppNavigator = createAppContainer (AppSwitchNavigator);
